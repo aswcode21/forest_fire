90c07e52c1b54e7ef260e941a186c1a550763203
aswcode21 <dealbusiness30@gmail.com>
2023-08-29 00:48:07 +0530

    first commmit

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-app = Flask(__name__)
+application = Flask(__name__)
 
 # import ridge regressor model and standard scaler pickle
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
